186fd6aa77e64f7f45527cd08a99c863b0649100
J.malassigne <J.malassigne22@gmail.com>
2021-06-07 19:47:05 +0200

    HTML et CSS left-aside

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,117 @@
       <div class="loading">
         <h1 class="loading__heading">julien malassigne</h1>
         <div class="loading__bar">
-          <div class="loading__bar-item" :class="{start: startProgress}"></div>
+          <div
+            class="loading__bar-item"
+            :class="{ start: startProgress }"
+          ></div>
         </div>
-        <p>{{progress}} %</p>
+        <p>{{ progress }} %</p>
       </div>
     </div>
     <div v-else class="main-page">
-      <aside class="aside-left">
-
+      <aside class="left-aside">
+        <header class="header">
+          <div class="header__img">
+            <img src="./assets/pp.jpg" alt="" />
+          </div>
+          <h1 class="header__name">julien malassigne</h1>
+          <h2>développeur web</h2>
+          <h3>full stack</h3>
+          <p></p>
+        </header>
+        <div class="main">
+          <div class="main__location">
+            <h3>Pays: <span>France</span></h3>
+            <h2>Ville: <span>Saint-Brieuc</span></h2>
+            <p>Age: <span>29</span></p>
+          </div>
+          <hr />
+          <div class="main__languages">
+            <div class="main__languages-item">
+              <div class="round">100%</div>
+              <p>Francais</p>
+            </div>
+            <div class="main__languages-item">
+              <div class="round second">75%</div>
+              <p>Anglais</p>
+            </div>
+            <div class="main__languages-item">
+              <div class="round third">50%</div>
+              <p>Italien</p>
+            </div>
+          </div>
+          <hr />
+          <div class="main__code">
+            <div class="main__code-item">
+              <h4>HTML <span>80%</span></h4>
+              <div class="bar">
+                <div class="bar-item first"></div>
+              </div>
+            </div>
+            <div class="main__code-item">
+              <h4>CSS <span>70%</span></h4>
+              <div class="bar">
+                <div class="bar-item second"></div>
+              </div>
+            </div>
+            <div class="main__code-item">
+              <h4>JavaScript <span>70%</span></h4>
+              <div class="bar">
+                <div class="bar-item third"></div>
+              </div>
+            </div>
+            <div class="main__code-item">
+              <h4>PHP <span>50%</span></h4>
+              <div class="bar">
+                <div class="bar-item fourth"></div>
+              </div>
+            </div>
+            <div class="main__code-item">
+              <h4>Wordpress <span>40%</span></h4>
+              <div class="bar">
+                <div class="bar-item fifth"></div>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="main__knowledge">
+            <h5><i class="fas fa-check"></i>Bootstrap</h5>
+            <h5><i class="fas fa-check"></i>SASS</h5>
+            <h5><i class="fas fa-check"></i>Git et Github</h5>
+            <h5><i class="fas fa-check"></i>VueJS</h5>
+            <h5><i class="fas fa-check"></i>NodeJS</h5>
+          </div>
+          <hr />
+          <div class="main__cv">
+            <a>Télécharger mon CV<i class="fas fa-download"></i></a>
+          </div>
+        </div>
+        <footer class="footer">
+          <a href="#"><i class="fab fa-linkedin"></i></a>
+          <a href="#"><i class="fab fa-github-square"></i></a>
+        </footer>
       </aside>
-      <Prez/>
-      <aside class="aside-right">
-        
-      </aside>
+      <Prez />
+      <aside class="right-aside"></aside>
     </div>
   </div>
 </template>
 
 <script>
-import Prez from './components/Prez.vue'
+import Prez from "./components/Prez.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Prez
+    Prez,
   },
   data() {
     return {
       isLoading: true,
       startProgress: false,
-      progress: 0
-    }
+      progress: 0,
+    };
   },
   mounted() {
     setTimeout(() => {
@@ -42,27 +122,93 @@ export default {
     }, 500);
 
     setInterval(() => {
-      if(this.progress < 100){
+      if (this.progress < 100) {
         this.progress++;
       }
-      }, 20);
+    }, 20);
 
     setTimeout(() => {
       this.isLoading = !this.isLoading;
     }, 3000);
-  }
-}
+  },
+};
 </script>
 
 <style lang='scss'>
+@import "./styles/_variables.scss";
+@import "./styles/_keyframes.scss";
 
-@import './styles/_variables.scss';
+/********** Reset **********/
 
-/********** Reset **********/  
+html,
+body,
+p,
+ol,
+ul,
+li,
+dl,
+dt,
+dd,
+blockquote,
+figure,
+fieldset,
+legend,
+textarea,
+pre,
+iframe,
+hr,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  padding: 0;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: 100%;
+  font-weight: normal;
+}
+ul {
+  list-style: none;
+}
+button,
+input,
+select {
+  margin: 0;
+}
+html {
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+img,
+video {
+  height: auto;
+  max-width: 100%;
+}
+iframe {
+  border: 0;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+td,
+th {
+  padding: 0;
+}
 
-html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}
-
-/********** Global styles **********/ 
+/********** Global styles **********/
 
 html {
   background-color: $background_1;
@@ -70,16 +216,28 @@ html {
 
 body {
   color: $white_para;
-  font-size: 16px;
-  font-weight: 500;
-  padding: 20px 0;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 25px;
+  padding: 20px;
+  font-family: "poppins", sans-serif;
 }
 
-h1,h2,h3,h4,h5,h6 {
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
   color: $white_heading;
 }
 
-/********** Loading **********/ 
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+/********** Loading **********/
 
 .loading {
   display: flex;
@@ -113,14 +271,273 @@ h1,h2,h3,h4,h5,h6 {
       }
     }
   }
-  
 }
 
-/********** Main Page **********/ 
+/********** Main Page **********/
 .main-page {
-  max-width: 300px;
+  display: flex;
+  max-width: 1500px;
+  height: calc(100vh - 40px);
   margin-left: auto;
   margin-right: auto;
 }
 
+/********** Asides **********/
+
+aside {
+  width: 280px;
+  background-color: $background_3;
+}
+
+/********** Left Aside **********/
+
+.left-aside {
+  height: 100%;
+  overflow-y: scroll; ///////////////////////////////////////// A verifier
+}
+
+.header {
+  position: fixed;
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 280px;
+  height: 230px;
+  padding: 25px 0;
+  background-color: rgba($color: $background_2, $alpha: 0.9);
+  text-transform: capitalize;
+
+  &__img {
+    position: relative;
+    height: 90px;
+    width: 90px;
+    border-radius: 50%;
+
+    &:after {
+      position: absolute;
+      bottom: 2px;
+      right: 2px;
+      content: "";
+      width: 20px;
+      height: 20px;
+      background-color: $yellow;
+      border-radius: 50%;
+      animation: pulse 1.5s infinite;
+    }
+
+    img {
+      width: inherit;
+      height: inherit;
+      border-radius: inherit;
+    }
+  }
+
+  &__name {
+    font-size: 14px;
+    font-weight: 600;
+    margin: 5px;
+  }
+  h2,
+  h3 {
+    color: $white_para;
+  }
+}
+
+.main {
+  margin-top: 230px;
+  margin-bottom: 50px;
+  padding: 20px 30px;
+
+  hr {
+    margin: 20px 0;
+    border-top: none;
+    border-color: rgba($color: #fff, $alpha: 0.2);
+  }
+
+  &__location {
+    color: $white_heading;
+
+    span {
+      float: right;
+      color: $white_para;
+    }
+  }
+
+  &__languages {
+    display: flex;
+    justify-content: space-between;
+
+    &-item {
+      text-align: center;
+
+      p {
+        color: $white_heading;
+      }
+
+      .round {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        border: 4px solid $yellow;
+        margin: 20px 0;
+
+        &.second:after {
+          content: "";
+          position: absolute;
+          background-color: $background_3;
+          clip-path: polygon(
+            22% 69%,
+            37% 43%,
+            62% 23%,
+            100% 12%,
+            100% 44%,
+            78% 52%,
+            65% 64%,
+            56% 82%,
+            54% 100%,
+            19% 100%
+          );
+          height: 30px;
+          width: 30px;
+          left: -10px;
+          top: -10px;
+        }
+        &.third:after {
+          content: "";
+          position: absolute;
+          background-color: $background_3;
+          clip-path: polygon(
+            22% 69%,
+            37% 43%,
+            62% 23%,
+            100% 12%,
+            100% 44%,
+            78% 52%,
+            65% 64%,
+            56% 82%,
+            54% 100%,
+            19% 100%
+          );
+          height: 30px;
+          width: 30px;
+          left: -10px;
+          top: -10px;
+        }
+        &.third:before {
+          content: "";
+          position: absolute;
+          background-color: $background_3;
+          clip-path: polygon(
+            22% 69%,
+            37% 43%,
+            62% 23%,
+            100% 12%,
+            100% 44%,
+            78% 52%,
+            65% 64%,
+            56% 82%,
+            54% 100%,
+            19% 100%
+          );
+          transform: rotate(-90deg) scale(1.1);
+          height: 30px;
+          width: 30px;
+          left: -10px;
+          bottom: -9px;
+        }
+      }
+    }
+  }
+
+  &__code {
+    &-item {
+      span {
+        float: right;
+        color: $white_para;
+      }
+      .bar {
+        width: 100%;
+        height: 3px;
+        border-radius: 1px;
+        margin: 7px 0 10px 0;
+
+        &-item {
+          height: 100%;
+          background-color: $yellow;
+
+          &.first {
+            width: 90%;
+          }
+          &.second {
+            width: 70%;
+          }
+          &.third {
+            width: 70%;
+          }
+          &.fourth {
+            width: 50%;
+          }
+          &.fifth {
+            width: 40%;
+          }
+        }
+      }
+    }
+  }
+
+  &__knowledge {
+    h5 {
+      color: $white_para;
+    }
+    i {
+      color: $yellow;
+      margin-right: 20px;
+    }
+  }
+
+  &__cv {
+    font-weight: 700;
+    font-size: 1rem;
+
+    a {
+      cursor: pointer;
+      transition: color .3s;
+
+      &:hover {
+        color: $white_heading
+      }
+    }
+
+    i {
+      margin-left: 10px;
+    }
+  }
+}
+
+.footer {
+  position: fixed;
+  bottom: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 280px;
+  height: 50px;
+  background-color: $background_2;
+
+  i {
+    font-size: 1.3rem;
+    margin: 0 15px;
+    transition: color .3s;
+
+    &:hover {
+      color: $white_heading;;
+    }
+  }
+}
 </style>
--- src/components/Prez.vue
+++ src/components/Prez.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-   <h1>Composant prez</h1>
+   <h1></h1>
   </div>
 </template>
 
